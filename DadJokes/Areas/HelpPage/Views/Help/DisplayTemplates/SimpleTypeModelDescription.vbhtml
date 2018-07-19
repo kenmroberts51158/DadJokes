9ad8a0422ed04ba363551e6ae397306a66425209
Kenneth Roberts <ken.roberts@sullivantire.com>
2018-07-19 16:29:06 -0400

    Add project files.

---
+++ DadJokes/Areas/HelpPage/Views/Help/DisplayTemplates/SimpleTypeModelDescription.vbhtml
@@ -0,0 +1,3 @@
+@Imports DadJokes.Areas.HelpPage.ModelDescriptions
+@ModelType SimpleTypeModelDescription
+@Model.Documentation
---
+++ DadJokes/Controllers/DadJokeController.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class DadJokeController
+        Inherits ApiController
+
+        Public Function GetValue() As String
+            Dim myDadJoke As DadJoke = DadJoke.GetInstance
+            Return myDadJoke.GetJoke()
+        End Function
+
+        Public Function GetValue(ByVal id As Integer) As String
+            Dim myDadJoke As DadJoke = DadJoke.GetInstance
+            Return myDadJoke.GetJoke(id)
+        End Function
+
+    End Class
+End Namespace
